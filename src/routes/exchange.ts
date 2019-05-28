77dff352a4596afdd20e4f2da510cc5fbc538893
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-05-28 15:34:02 +1000

    feat(exchange): initial exchange data loading

--- src/routes/exchange.ts
+++ src/routes/exchange.ts
@@ -1,3 +1,18 @@
+import { SteemEngine } from '../services/steem-engine';
+import { autoinject } from 'aurelia-framework';
+import { environment } from 'environment';
+
+@autoinject()
 export class Exchange {
-    
+    private currentToken: string;
+
+    constructor(private se: SteemEngine) {
+
+    }
+
+    activate({symbol}) {
+        this.currentToken = symbol;
+
+        this.se.loadTokens();
+    }
 }
