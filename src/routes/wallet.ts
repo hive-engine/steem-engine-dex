095e0f4c31151f28e6ff5de77fe71c61c2788d0d
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-06-10 14:32:42 +1000

    feat(wallet): display user token balances

--- src/routes/wallet.ts
+++ src/routes/wallet.ts
@@ -1,3 +1,23 @@
+import { autoinject } from 'aurelia-framework';
+import { SteemEngine } from 'services/steem-engine';
+
+@autoinject()
 export class Wallet {
+    private balances: BalanceInterface[];
+    private tokenTable: HTMLTableElement;
     
+    constructor(private se: SteemEngine) {
+
+    }
+
+    attached() {
+        // @ts-ignore
+        $(this.tokenTable).DataTable();
+    }
+    
+    async activate() {       
+        this.balances = await this.se.loadBalances();
+
+        console.log(this.balances);
+    }
 }
