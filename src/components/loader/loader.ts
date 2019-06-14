3b4fc354f6e925b2edc73b67448f1d5cc5af64b8
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-06-14 16:48:47 +1000

    feat(loader): brand new cross-platform loader

---
+++ src/components/loader/loader.ts
@@ -0,0 +1,30 @@
+import { customElement, autoinject, bindable } from 'aurelia-framework';
+
+import styles from './loader.css';
+
+@autoinject()
+@customElement('loader')
+export class Loader {
+    private element: HTMLElement;
+    private styles = styles;
+
+    static inject = [Element];
+
+    @bindable loading = false;
+
+    constructor(element: HTMLElement) {
+        this.element = element;
+    }
+
+    loadingChanged(newVal) {
+        if (newVal) {
+            this.element.style.opacity = '1';
+            this.element.style.display = 'block';
+            document.body.style.overflow = 'hidden';
+        } else {
+            this.element.style.opacity = '0';
+            this.element.style.display = 'none';
+            document.body.style.overflow = 'unset';
+        }
+    }
+}
