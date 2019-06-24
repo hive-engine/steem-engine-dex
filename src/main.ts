0e5b348ba7bc40bb4ccdf7dd3e8bc5d238e98af4
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-06-24 15:25:07 +1000

    feat(app): main style should go through main loader

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ import '!style-loader!css-loader!bootstrap/dist/css/bootstrap.min.css';
 import '!style-loader!css-loader!font-awesome/css/font-awesome.min.css';
 import '!style-loader!css-loader!izitoast/dist/css/iziToast.css';
 import '!style-loader!css-loader!./styles/toast.css';
-import '!style-loader!css-loader!./styles/main.css';
+import './styles/main.css';
 
 import modalCss from '!style-loader!css-loader!./styles/modal.css';
 
