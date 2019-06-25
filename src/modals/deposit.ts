f357f1c348899dcceb5ab48eef153883522ebc2c
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-06-25 10:40:49 +1000

    feat(modals): get current user to get balance

--- src/modals/deposit.ts
+++ src/modals/deposit.ts
@@ -1,11 +1,22 @@
+import { Store } from 'aurelia-store';
 import { SteemEngine } from 'services/steem-engine';
 import { DialogController } from 'aurelia-dialog';
 import { autoinject } from 'aurelia-framework';
+import { State } from 'store/state';
+import { pluck } from 'rxjs/operators';
 
 @autoinject()
 export class DepositModal {
-    constructor(private controller: DialogController, private se: SteemEngine) {
+    private user: any;
+
+    constructor(private controller: DialogController, private se: SteemEngine, private store: Store<State>) {
         this.controller.settings.lock = false;
         this.controller.settings.centerHorizontalOnly = true;
+    }
+
+    bind() {
+        this.store.state.pipe(pluck('account')).subscribe((user: any) => {
+            this.user = user;
+        });
     }
 }
