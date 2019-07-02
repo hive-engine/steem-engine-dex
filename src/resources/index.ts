efb6b023d98e1319af98927632e845683201b35a
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-07-03 09:42:08 +1000

    feat(resources): capitalise value converter

--- src/resources/index.ts
+++ src/resources/index.ts
@@ -5,6 +5,7 @@ export function configure(config: FrameworkConfiguration) {
     config.globalResources([
         PLATFORM.moduleName('./attributes/class'),
         PLATFORM.moduleName('./attributes/flatpickr'),
+        PLATFORM.moduleName('./value-converters/capitalise'),
         PLATFORM.moduleName('./value-converters/comma'),
         PLATFORM.moduleName('./value-converters/large-number'),
         PLATFORM.moduleName('./value-converters/to-fixed'),
