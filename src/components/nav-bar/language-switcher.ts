54203f1f08fc35ca6650ab2ad85f7640f87bd294
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-07-04 09:36:28 +1000

    feat(nav-bar): save language choice to user

--- src/components/nav-bar/language-switcher.ts
+++ src/components/nav-bar/language-switcher.ts
@@ -1,5 +1,8 @@
+import { SteemEngine } from './../../services/steem-engine';
 import { customElement, autoinject, containerless } from 'aurelia-framework';
 import { I18N } from 'aurelia-i18n';
+
+import firebase from 'firebase/app';
 
 import 'flag-icon-css/css/flag-icon.css';
 
@@ -15,12 +18,34 @@ export class LanguageSwitcher {
 
     private selectedLanguage = this.languages[0];
 
-    constructor(private i18n: I18N) {
+    constructor(private i18n: I18N, private se: SteemEngine) {
 
+    }
+
+    async attached() {
+        try {
+            const doc = await firebase.firestore().collection('users').doc(this.se.getUser()).get();
+
+            if (doc.exists) {
+                const user = doc.data();
+    
+                if (user.language) {
+                    this.setLanguage(user.language);
+                }
+            }
+        } catch {
+            // Nothing
+        }
     }
 
     setLanguage(language: any) {
         this.selectedLanguage = language;
         this.i18n.setLocale(language.value);
+
+        const userRef = firebase.firestore().collection('users').doc(this.se.getUser());
+
+        userRef.set({ language: this.selectedLanguage }, {
+            merge: true
+        });
     }
 }
