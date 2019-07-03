63cd98718aaf9c742e17044c32e5812dfd96e4ea
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-07-03 20:04:25 +1000

    feat(settings): load the user

--- src/routes/settings.ts
+++ src/routes/settings.ts
@@ -1,5 +1,23 @@
+import { SteemEngine } from './../services/steem-engine';
 import firebase from 'firebase/app';
+import { autoinject } from 'aurelia-framework';
 
+@autoinject()
 export class Settings {
-    
+    private user = null;
+
+    constructor(private se: SteemEngine) {
+
+    }
+
+    async activate() {
+        console.log(this.se.getUser());
+        const doc = await firebase.firestore().collection('users').doc(this.se.getUser()).get();
+
+        if (doc.exists) {
+            this.user = doc.data();
+        }
+
+        console.log(this.user);
+    }
 }
