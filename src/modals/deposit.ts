0f846b7ec494ab8d986270470eae252f5e73b43f
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-07-04 16:25:12 +1000

    fix(deposit): fix the deposit modal duplicate balances and logic

--- src/modals/deposit.ts
+++ src/modals/deposit.ts
@@ -2,15 +2,11 @@ import { Store } from 'aurelia-store';
 import { SteemEngine } from 'services/steem-engine';
 import { DialogController } from 'aurelia-dialog';
 import { autoinject, TaskQueue } from 'aurelia-framework';
-import { State } from 'store/state';
-import { pluck } from 'rxjs/operators';
-import { Subscription } from 'rxjs';
 import { environment } from 'environment';
 
 @autoinject()
 export class DepositModal {
     private environment = environment;
-    private subscription: Subscription;
     private user: any;
     private token: any = null;
     private depositInfo: any = null;
@@ -18,21 +14,9 @@ export class DepositModal {
     
     private amount = '0.000';
 
-    constructor(private controller: DialogController, private se: SteemEngine, private store: Store<State>, private taskQueue: TaskQueue) {
+    constructor(private controller: DialogController, private se: SteemEngine, private taskQueue: TaskQueue) {
         this.controller.settings.lock = false;
         this.controller.settings.centerHorizontalOnly = true;
-    }
-
-    bind() {
-        this.subscription = this.store.state.pipe(pluck('account')).subscribe((user: any) => {
-            this.user = user;
-        });
-    }
-
-    unbind() {
-        if (this.subscription) {
-            this.subscription.unsubscribe();
-        }
     }
 
     tokenSelected() {
