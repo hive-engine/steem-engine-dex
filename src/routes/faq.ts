419c1978ce63f5c690a257695b0261165d36c73c
horlorlahdeh <horlorlahdeh@gmail.com>
2019-10-05 14:25:17 +0100

    Faq page now done with rooms for adding additioinal question categories. Some additional changes to the homepage

--- src/routes/faq.ts
+++ src/routes/faq.ts
@@ -1,3 +1,11 @@
+import styles from "./faq.module.css";
+
 export class Faq {
-    
+    private styles = styles;
+
+    // To hide or show components conditionally
+    handleClick(e) {
+        $('.faq-detail').css('display', 'none');
+        $('#'+e+'-question-tab').css('display', 'block');
+    }
 }
