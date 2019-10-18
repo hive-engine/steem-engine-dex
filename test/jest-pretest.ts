1c1fef2a48fedea7c46bf09c9646c05ce326c9c9
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-10-18 23:39:15 +1000

    test(setup): global container created

--- test/jest-pretest.ts
+++ test/jest-pretest.ts
@@ -4,5 +4,9 @@ import 'aurelia-polyfills';
 import {Options} from 'aurelia-loader-nodejs';
 import {globalize} from 'aurelia-pal-nodejs';
 import * as path from 'path';
+import { Container } from 'aurelia-framework';
 Options.relativeToDir = path.join(__dirname, 'unit');
 globalize();
+
+const container = new Container();
+container.makeGlobal();
