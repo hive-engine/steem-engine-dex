87e415d4d644c029f4bcb1a3c2a7b351d8b5573b
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-10-18 17:24:38 +1000

    test(unit): add stage helper method for tests with localisation

--- test/unit/helpers.ts
+++ test/unit/helpers.ts
@@ -1,6 +1,56 @@
 import { Store, StoreOptions, StateHistory } from 'aurelia-store';
 import { Aurelia, PLATFORM } from 'aurelia-framework';
 import { Backend, TCustomAttribute } from 'aurelia-i18n';
+import { ComponentTester, StageComponent } from 'aurelia-testing';
+
+export function stageComponent(view: string, resources: string[] = [], viewModel = {}) {
+    let component: ComponentTester;
+  
+    component = StageComponent.withResources(resources)
+      .boundTo(viewModel)
+      .inView(view);
+  
+    // @ts-ignore
+    component.bootstrap((aurelia: Aurelia) => {
+      aurelia.use.standardConfiguration();
+      aurelia.use.plugin(PLATFORM.moduleName('aurelia-i18n'), (instance) => {
+        const aliases = ['t', 'i18n'];
+  
+        TCustomAttribute.configureAliases(aliases);
+  
+        // register backend plugin
+        instance.i18next.use(Backend.with(aurelia.loader));
+        const config = {
+            resources: {
+                en: {
+                    translation: {
+                        hello: undefined,
+                    },
+                },
+            },
+            skipTranslationOnMissingKey: true,
+        };
+  
+        return instance.setup({
+            attributes: aliases,
+            backend: {
+                loadPath: './locales/{{lng}}/{{ns}}.json',
+            },
+            debug: false,
+            defaultNS: 'translation',
+            fallbackLng: 'en',
+            interpolation: {
+                prefix: '{{',
+                suffix: '}}',
+            },
+            lng: 'en', ...config});
+    });
+  
+      aurelia.container.registerInstance(Element, document.createElement('div'));
+    });
+  
+    return component;
+  }
 
 export function createStore<T>(state: T, withUndo = false) {
   const options = withUndo ? { history: { undoable: true } } : {};
