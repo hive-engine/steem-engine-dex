4a98d65b78a95916e0a98a377efbff43e86b27e6
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-10-26 22:29:27 +1000

    feat(resources): authorize pipeline should check route for roles

--- src/resources/pipeline-steps/authorize.ts
+++ src/resources/pipeline-steps/authorize.ts
@@ -1,26 +1,44 @@
-import { Redirect } from 'aurelia-router';
+import { Redirect, RouteConfig } from 'aurelia-router';
 import firebase from 'firebase/app';
+import { autoinject } from 'aurelia-framework';
+import { SteemEngine } from 'services/steem-engine';
 
+@autoinject()
 export class AuthorizeStep {
-  run(navigationInstruction, next) {
-    return new Promise((resolve, reject) => {
-        firebase.auth().onAuthStateChanged(user => {        
-            let currentRoute = navigationInstruction.config;
-            
-            let loginRequired = currentRoute.auth === true;
-            
-            if (!user && loginRequired === true) {
-                return resolve(next.cancel(new Redirect('')));
-            }
-            
-            let publicOnly = currentRoute.publicOnly === true;
-    
-            if (user && publicOnly === true) {
-                return resolve(next.cancel(new Redirect('wallet')));
-            }
-            
-            return resolve(next());
+    constructor(private se: SteemEngine) {
+
+    }
+
+    run(navigationInstruction, next) {
+        return new Promise((resolve, reject) => {
+            firebase.auth().onAuthStateChanged(async (user) => {        
+                let currentRoute: RouteConfig = navigationInstruction.config;
+                
+                let loginRequired = currentRoute.auth === true;
+                
+                if (!user && loginRequired === true) {
+                    return resolve(next.cancel(new Redirect('')));
+                }
+                
+                let publicOnly = currentRoute.publicOnly === true;
+        
+                if (user && publicOnly === true) {
+                    return resolve(next.cancel(new Redirect('wallet')));
+                }
+
+                // The current route has settings and roles
+                if (currentRoute.settings && currentRoute.settings.roles) {
+                    const doc = await firebase.firestore().collection('users').doc(this.se.getUser()).get();
+                    const data = doc.data();
+
+                    // If the current user doesn't have every role configured in the route
+                    if (!data.roles.every(r => currentRoute.settings.roles.includes(r))) {
+                        return resolve(next.cancel(new Redirect('wallet')));
+                    }
+                }
+                
+                return resolve(next());
+            });
         });
-    });
-  }
+    }
 }
