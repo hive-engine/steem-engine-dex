679b7fd581c7db3ae79bf71f53f72592cd9f9238
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-10-27 19:44:21 +1000

    feat(admin): load kyc items

--- src/routes/admin/kyc.ts
+++ src/routes/admin/kyc.ts
@@ -1,3 +1,12 @@
+import firebase from 'firebase/app';
+
 export class AdminKyc {
-    
+    private kycItems = [];
+
+    async activate() {
+        const passportPendingUsers = await firebase.firestore().collection('users').where('passportPending', '==', true).get();
+        const selfiePendingUsers = await firebase.firestore().collection('users').where('selfiePending', '==', true).get();
+
+        console.log(passportPendingUsers, selfiePendingUsers);
+    }
 }
