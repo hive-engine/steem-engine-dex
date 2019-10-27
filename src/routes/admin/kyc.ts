0d3da4c16667db9dd27def5498aa83be9540a01e
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-10-27 21:32:06 +1000

    feat(admin): display kyc pending items

--- src/routes/admin/kyc.ts
+++ src/routes/admin/kyc.ts
@@ -1,12 +1,22 @@
 import firebase from 'firebase/app';
 
+import styles from './kyc.module.css';
+
 export class AdminKyc {
+    private styles = styles;
+
     private kycItems = [];
 
     async activate() {
-        const passportPendingUsers = await firebase.firestore().collection('users').where('passportPending', '==', true).get();
-        const selfiePendingUsers = await firebase.firestore().collection('users').where('selfiePending', '==', true).get();
+        const pendingKycUsers = await firebase.firestore().collection('users')
+            .where('kyc.passportPending', '==', true)
+            .where('kyc.selfiePending', '==', true)
+            .get();
 
-        console.log(passportPendingUsers, selfiePendingUsers);
+        console.log(pendingKycUsers);
+
+        if (pendingKycUsers.docs) {
+            this.kycItems = pendingKycUsers.docs.map(doc => ({id: doc.id, ...doc.data()}));
+        }
     }
 }
