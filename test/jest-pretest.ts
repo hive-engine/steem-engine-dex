4f0f3b55f160ad24dc4ff655536704edbf5effc7
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-10-29 22:31:40 +1000

    feat(test): mock localstorage and navigator for tests

--- test/jest-pretest.ts
+++ test/jest-pretest.ts
@@ -6,6 +6,23 @@ customGlobal.fetchMock = customGlobal.fetch;
 
 (global as any).Blob = jest.fn();
 
+(global as any).localStorage = {
+    values: [],
+    getItem: jest.fn().mockImplementation((key: string) => {
+        return (global as any).localStorage.values[key] || null;
+    }),
+    setItem: jest.fn().mockImplementation((key: string, value: string) => {
+        (global as any).localStorage.values[key] = value;
+    }),
+    removeItem: jest.fn().mockImplementation((key: string) => {
+        delete (global as any).localStorage.values[key];
+    }),
+};
+
+(global as any).navigator = {
+    
+};
+
 import 'aurelia-polyfills';
 import { Options } from 'aurelia-loader-nodejs';
 import { globalize } from 'aurelia-pal-nodejs';
