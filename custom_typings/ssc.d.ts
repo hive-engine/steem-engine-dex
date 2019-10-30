fbf7ad9e61948294f2739dc6aea397932bcb5a98
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-10-30 16:56:29 +1000

    feat(typings): token and metric typings for ssc

--- custom_typings/ssc.d.ts
+++ custom_typings/ssc.d.ts
@@ -4,4 +4,47 @@ declare namespace SSC {
     }
 }
 
+interface IsscToken {
+    _id: number;
+    circulatingSupply: string;
+    delegationEnabled: boolean;
+    highestBid: number;
+    issuer: string;
+    lastPrice: number;
+    lowestAsk: number;
+    marketCap: number;
+    maxSupply: number;
+    metadata: {
+        desc: string;
+        icon: string;
+        url: string;
+    } | null;
+    name: string;
+    numberTransactions: number;
+    precision: number;
+    priceChangePercent: number;
+    priceChangeSteem: number;
+    stakingEnabled: boolean;
+    supply: number;
+    symbol: string;
+    totalStaked: string;
+    undelegationCooldown: number;
+    unstakingCooldown: number;
+    usdValue: string;
+    volume: number;
+}
+
+interface IsscMetric {
+    highestBid: string;
+    lastDayPrice: string;
+    lastDayPriceExpiration: number;
+    lastPrice: string;
+    lowestAsk: string;
+    priceChangePercent: string;
+    priceChangeSteem: string;
+    symbol: string;
+    volume: string;
+    volumeExpiration: number;
+}
+
 declare const SSC: SSC.SSC;
