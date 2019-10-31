bfdcd85345422e6cf06fadc87e0058ab2360e493
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-10-31 12:15:56 +1000

    feat(admin): load and bind administration panel settings

--- src/routes/admin/settings.ts
+++ src/routes/admin/settings.ts
@@ -1,3 +1,13 @@
+import firebase from 'firebase/app';
+
 export class AdminSettings {
-    
+    private settings;
+
+    async activate() {
+        const settings = await firebase.firestore().collection('admin').doc('settings').get();
+
+        if (settings.exists) {
+            this.settings = settings.data();
+        }
+    }
 }
