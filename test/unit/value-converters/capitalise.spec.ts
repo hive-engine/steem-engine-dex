a56de5849a992f84d035540f6137ef711186a2dd
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-11-10 09:12:20 +1000

    test(capitalise): unit test capitalise value converter

---
+++ test/unit/value-converters/capitalise.spec.ts
@@ -0,0 +1,26 @@
+import { Capitalise } from './../../../src/resources/value-converters/capitalise';
+
+describe('Auth Filter', () => {
+    let sut: Capitalise;
+
+    beforeEach(() => {
+        sut = new Capitalise();
+    })
+
+    test('invalid value should return passed value', () => {
+        expect(sut.toView(null)).toBeNull();
+    });
+
+    test('number passed instead of string should return empty string', () => {
+        expect(sut.toView(1234)).toBe('');
+    });
+
+    test('should capitalise only first letter', () => {
+        expect(sut.toView('beggars')).toBe('Beggars');
+    });
+
+    test('should capitalise only first letter of multi word value', () => {
+        expect(sut.toView('beggars is here')).toBe('Beggars is here');
+    });
+
+});
