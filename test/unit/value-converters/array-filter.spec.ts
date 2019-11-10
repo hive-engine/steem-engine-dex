1ae54ce7badffa6da3d5c017b890d530180508e0
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-11-10 21:06:11 +1000

    test(array-filter): more test cases for filtering and sorting

--- test/unit/value-converters/array-filter.spec.ts
+++ test/unit/value-converters/array-filter.spec.ts
@@ -35,4 +35,40 @@ describe('Array Filter', () => {
         expect(result).toHaveLength(0);
     });
 
+    test('filter array with empty term value', () => {
+        const mockData = [
+            { username: 'beggars', blockchain: 'steem' },
+            { username: 'aggroed', blockchain: 'steem' },
+            { username: 'bait002', blockchain: 'steem' }
+        ];
+
+        const result = sut.toView(mockData, { search: 'username', term: '' });
+
+        expect(result).toHaveLength(0);
+    });
+
+    test('filter array and sort array with no provided sort order', () => {
+        const mockData = [
+            { username: 'beggars', blockchain: 'steem' },
+            { username: 'aggroed', blockchain: 'steem' },
+            { username: 'bait002', blockchain: 'steem' }
+        ];
+
+        const result = sut.toView(mockData, { search: 'blockchain', term: 'steem', sort: { key: 'username' } });
+
+        expect(result[0].username).toBe('aggroed');
+    });
+
+    test('filter array and sort array and descending sort order', () => {
+        const mockData = [
+            { username: 'beggars', blockchain: 'steem' },
+            { username: 'aggroed', blockchain: 'steem' },
+            { username: 'bait002', blockchain: 'steem' }
+        ];
+
+        const result = sut.toView(mockData, { search: 'blockchain', term: 'steem', sort: { key: 'username', direction: 'desc' } });
+
+        expect(result[1].username).toBe('bait002');
+    });
+
 });
