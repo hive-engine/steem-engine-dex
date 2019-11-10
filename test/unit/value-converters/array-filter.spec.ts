9f5808bfbc55e311090d993286ac2bb5e74962be
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-11-10 15:13:15 +1000

    test(array-filter): begin writing test of array filter

--- test/unit/value-converters/array-filter.spec.ts
+++ test/unit/value-converters/array-filter.spec.ts
@@ -8,7 +8,31 @@ describe('Array Filter', () => {
     })
 
     test('invalid value should return passed value', () => {
-        expect(sut.toView(null, '', '')).toBeNull();
+        expect(sut.toView(null, { search: '', term: 'yes' })).toBeNull();
+    });
+
+    test('filter array based on property name', () => {
+        const mockData = [
+            { username: 'beggars', blockchain: 'steem' },
+            { username: 'aggroed', blockchain: 'steem' },
+            { username: 'bait002', blockchain: 'steem' }
+        ];
+
+        const result = sut.toView(mockData, { search: 'username', term: 'beggars' });
+
+        expect(result).toEqual([{ username: 'beggars', blockchain: 'steem' }]);
+    });
+
+    test('filter array based on property name and case sensitivity', () => {
+        const mockData = [
+            { username: 'beggars', blockchain: 'steem' },
+            { username: 'aggroed', blockchain: 'steem' },
+            { username: 'bait002', blockchain: 'steem' }
+        ];
+
+        const result = sut.toView(mockData, { search: 'username', term: 'BEGGARS', caseSensitive: true });
+
+        expect(result).toEqual([{ username: 'beggars', blockchain: 'steem' }]);
     });
 
 });
