b1f2e3720267f5176924d35c14336b7fc3a1143b
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-11-11 09:01:26 +1000

    test(store): testing store methods

---
+++ test/unit/store.spec.ts
@@ -0,0 +1,35 @@
+import { Store } from 'aurelia-store';
+import { Container } from 'aurelia-framework';
+import { createStore } from './helpers';
+
+const container: Container = new Container().makeGlobal();
+const store: any = createStore({username: 'aggroed', property: 'something'});
+container.registerInstance(Store, store);
+
+store.registerAction('test', (state: any, username: string) => {
+    const newState = { ...state };
+
+    newState.username = username;
+
+    return newState;
+});
+
+import { getStateOnce, getCurrentState } from 'store/store';
+
+describe('Store', () => {
+    test('gets state once', async () => {
+        const state: any = await getStateOnce();
+
+        expect(state).toEqual({username: 'aggroed', property: 'something'});
+
+        store.dispatch('test', 'beggars');
+
+        expect(state.username).toBe('aggroed');
+    });
+
+    test('get current state object directly', () => {
+        store.dispatch('test', 'aggroed');
+
+        expect(getCurrentState()).toEqual({username: 'aggroed', property: 'something'});
+    })
+});
