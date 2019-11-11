cf89a476c0c5b3b6024b415db3bf090ffc9f2630
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-11-11 11:36:09 +1000

    feat(kyc-view): allow kyc to be approved

--- src/routes/admin/kyc-view.ts
+++ src/routes/admin/kyc-view.ts
@@ -1,12 +1,26 @@
-import { Redirect, RouteConfig } from 'aurelia-router';
+import { SteemEngine } from 'services/steem-engine';
+import { computedFrom, autoinject } from 'aurelia-framework';
+import { Redirect, RouteConfig, AppRouter } from 'aurelia-router';
 
 import 'firebase/storage';
 import firebase from 'firebase/app';
 
+@autoinject()
 export class AdminKycView {
     private user;
     private passportImage;
     private selfieImage;
+    private approve = {
+        code: false,
+        selfieQuality: false,
+        selfieDate: false,
+        passportDate: false,
+        passportDetails: false
+    };
+
+    constructor(private se: SteemEngine, private router: AppRouter) {
+
+    }
 
     async canActivate(params: { uid: string }, routeConfig: RouteConfig) {
         if (!params.uid) {
@@ -19,8 +33,6 @@ export class AdminKycView {
             this.user = { id: user.id, ...user.data() };
 
             routeConfig.navModel.setTitle(`KYC > ${this.user.id}`);
-
-            console.info(`KYC View`, this.user);
         }
     }
 
@@ -37,6 +49,25 @@ export class AdminKycView {
         // eslint-disable-next-line no-undef
         if (this.user?.selfie?.filename) {
             this.selfieImage = await userUploads.child(`${this.user.id}/${this.user.selfie.filename}`).getDownloadURL();
+        }
+    }
+
+    @computedFrom('approve.code', 'approve.selfieQuality', 'approve.selfieDate', 'approve.passportDate', 'approve.passportDetails')
+    get canApprove() {
+        return Object.keys(this.approve).every(prop => this.approve[prop]);
+    }
+
+    async approveKyc() {
+        const userRef = firebase.firestore().collection('users').doc(this.se.getUser());
+
+        try {
+            await userRef.set({ kyc: { passportPending: false, passportVerified: true, selfiePending: false, selfieVerified: true, verified: true } }, {
+                merge: true
+            });
+
+            this.router.navigate('/admin/kyc');
+        } catch (e) {
+            console.error(e);
         }
     }
 }
