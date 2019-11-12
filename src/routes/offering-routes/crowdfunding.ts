5f5c94a346c1e30c3a0c505f0802f83e0e31ab14
horlorlahdeh <horlorlahdeh@gmail.com>
2019-11-12 07:58:37 +0100

    Crowdfunding; landing page

--- src/routes/offering-routes/crowdfunding.ts
+++ src/routes/offering-routes/crowdfunding.ts
@@ -1,5 +1,80 @@
 ﻿import styles from "./crowdfunding.module.css";
+import { customElement, bindable } from "aurelia-framework";
+import { faClock } from "@fortawesome/pro-solid-svg-icons";
+import { slick } from "slick-carousel/slick/slick";
+import { typewriter } from "typewriter-effect/dist/core";
+import { Callback } from "i18next";
+
 
 export class Crowdfunding {
+    @bindable iconClock = faClock;
+
+    private Slick = slick;
+    private Typewriter = typewriter;
     private styles = styles;
+
+    addClass(clss: string) {
+        var app = document.getElementById('app');
+
+        var typewriter = new typewriter(app, {
+            loop: true
+        });
+
+        typewriter.typeString('Hello World!')
+            .pauseFor(2500)
+            .deleteAll()
+            .typeString('Strings can be removed')
+            .pauseFor(2500)
+            .deleteChars(7)
+            .typeString('<strong>altered!</strong>')
+            .pauseFor(2500)
+            .start();
+        console.log(clss);
+    }
+
+    attached() {
+        $(".exploration-cards").slick({
+            dots: true,
+            arrows: true,
+            infinite: false,
+            speed: 300,
+            autoplay: true,
+            slidesToShow: 3,
+            slidesToScroll: 1,
+            responsive: [
+                {
+                    breakpoint: 1024,
+                    settings: {
+                        slidesToShow: 3,
+                        slidesToScroll: 3,
+                        infinite: true,
+                        dots: true
+                    }
+                },
+                {
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2
+                    }
+                },
+                {
+                    breakpoint: 480,
+                    settings: {
+                        slidesToShow: 1,
+                        slidesToScroll: 1
+                    }
+                }
+                // You can unslick at a given breakpoint now by adding:
+                // settings: "unslick"
+                // instead of a settings object
+            ]
+        });
+        
+    }
+
+
+        
+    
+
 }
