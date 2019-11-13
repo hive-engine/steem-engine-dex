0d9df75a9ab50ec11d939db703888049acc3024a
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-11-13 12:05:57 +1000

    fix(wallet): pending withdrawals check for if statement

--- src/routes/wallet/pending-withdrawals.ts
+++ src/routes/wallet/pending-withdrawals.ts
@@ -1,16 +1,18 @@
-import { pluck } from 'rxjs/operators';
+import { first, pluck } from 'rxjs/operators';
 import { State } from './../../store/state';
 import { dispatchify, Store } from 'aurelia-store';
 import { Redirect } from 'aurelia-router';
 import { SteemEngine } from 'services/steem-engine';
 import { autoinject } from 'aurelia-framework';
 import { getPendingWithdrawals } from 'store/actions';
+import { Observable } from 'rxjs';
 
 @autoinject()
 export class OpenOrders {
     private loadingWithdrawals = false;
+    private hasWithdrawals = false;
 
-    private withdrawalObs;
+    private withdrawalObs: Observable<any[]>;
     
     constructor(private se: SteemEngine, private store: Store<State>) {
 
@@ -32,5 +34,6 @@ export class OpenOrders {
 
     bind() {
         this.withdrawalObs = this.store.state.pipe(pluck('pendingWithdrawals'));
+        this.withdrawalObs.pipe(first()).subscribe(withdrawals => this.hasWithdrawals = withdrawals.length > 0);
     }
 }
