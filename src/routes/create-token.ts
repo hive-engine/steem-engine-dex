686f3d0e62e91174b12825e266c7a219ed0dddfe
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-11-15 15:15:29 +1000

    feat(create-token): validation

--- src/routes/create-token.ts
+++ src/routes/create-token.ts
@@ -1,14 +1,28 @@
+import { BootstrapFormRenderer } from './../resources/bootstrap-form-renderer';
 import { loadAccountBalances } from 'store/actions';
 import { State } from './../store/state';
 import { Store, dispatchify } from 'aurelia-store';
+import { ValidationController, ValidationControllerFactory, validateTrigger, ValidationRules } from 'aurelia-validation';
 import { autoinject } from 'aurelia-framework';
 
 @autoinject()
 export class CreateToken {
+    private renderer: BootstrapFormRenderer;
+    private controller: ValidationController;
     private engBalance;
 
-    constructor(private store: Store<State>) {
+    private tokenName;
+    private precision;
+    private symbol;
+    private maxSupply;
 
+    constructor(private controllerFactory: ValidationControllerFactory, private store: Store<State>) {
+        this.controller = controllerFactory.createForCurrentScope();
+
+        this.controller.validateTrigger = validateTrigger.manual;
+
+        this.renderer = new BootstrapFormRenderer();
+        this.controller.addRenderer(this.renderer);
     }
 
     async activate() {
@@ -26,5 +40,30 @@ export class CreateToken {
                 }
             }
         });
+
+        ValidationRules
+            .ensure('tokenName')
+                .required()
+                .withMessageKey('errors:requiredAlphaNumericSpaces')
+            .ensure('precision')
+                .required()
+                .between(0, 8)
+                .withMessageKey('errors:betweenZeroAndEight')
+            .ensure('symbol')
+                .required()
+                .minLength(1)
+                .maxLength(10)
+                .withMessageKey('errors:required')
+            .ensure('maxSupply')
+                .required()
+                .between(1, 9007199254740991)
+                .withMessageKey('errors:maxSupply')
+        .on(CreateToken);
+    }
+
+    public async createToken() {
+        const validationResult = await this.controller.validate();
+
+        console.log(validationResult);
     }
 }
