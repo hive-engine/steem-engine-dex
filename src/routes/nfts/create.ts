c4d865a3377f6c19d021d04f31b955479d898358
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-11-19 15:25:54 +1000

    feat(nfts): creation screen working with conditional validation

--- src/routes/nfts/create.ts
+++ src/routes/nfts/create.ts
@@ -18,10 +18,11 @@ export class CreateNft {
     private tokenCreationFee;
 
     private tokenName = null;
-    private precision = null;
     private symbol = null;
     private maxSupply = null;
     private url = null;
+    private authorisedIssuingAccounts: any[] = [];
+    private authorisedIssuingContracts: any[] = [];
 
     private state: State;
 
@@ -37,8 +38,8 @@ export class CreateNft {
     async activate() {
         await dispatchify(loadAccountBalances)();
 
-        const data = await query(`query { nftParams { tokenCreationFee } }`);
-        const tokenCreationFee = data?.data?.nftParams?.[0]?.tokenCreationFee ?? 100;
+        const data = await query(`query { nftParams { nftCreationFee } }`);
+        const tokenCreationFee = data?.data?.nftParams?.[0]?.nftCreationFee ?? 100;
 
         console.log(tokenCreationFee);
 
@@ -51,7 +52,7 @@ export class CreateNft {
 
             // eslint-disable-next-line no-undef
             if (state?.account?.balances?.length) {
-                const engToken = state.account.balances.find(token => token.symbol === 'ENG');
+                const engToken = state.account.balances.find(token => token.symbol === state.settings.nativeToken);
 
                 if (engToken) {
                     this.engBalance = engToken.balance;
@@ -61,21 +62,17 @@ export class CreateNft {
 
         ValidationRules.ensure('tokenName')
             .required()
-            .withMessageKey('errors:required')
+                .withMessageKey('errors:required')
+            .maxLength(50)
+                .withMessageKey('errors:maximumLength50')
             .satisfies((value: string) => {
                 return value?.match(/^[a-zA-Z0-9 ]*$/)?.length > 0 ?? false;
             })
-            .withMessageKey('errors:requiredAlphaNumericSpaces')
-
-            .ensure('precision')
-            .required()
-            .withMessageKey('errors:required')
-            .satisfies((value: string) => parseInt(value) >= 0 && parseInt(value) <= 8)
-            .withMessageKey('errors:betweenZeroAndEight')
+                .withMessageKey('errors:requiredAlphaNumericSpaces')
 
             .ensure('symbol')
             .required()
-            .withMessageKey('errors:required')
+                .withMessageKey('errors:required')
             .satisfies((value: string) => {
                 const isUppercase = value === value?.toUpperCase() ?? false;
                 const validLength = (value?.length >= 3 && value?.length <= 10) ?? false;
@@ -83,28 +80,65 @@ export class CreateNft {
 
                 return isUppercase && validLength && validString;
             })
-            .withMessageKey('errors:symbolValid')
+                .withMessageKey('errors:symbolValid')
 
             .ensure('maxSupply')
-            .required()
-            .withMessageKey('errors:required')
-            .satisfies((value: string) => value && parseInt(value) >= 1 && parseInt(value) <= 9007199254740991)
-            .withMessageKey('errors:maxSupply')
+            .satisfies((value: string) => {
+                return this.maxSupply !== null && this.maxSupply.toString().length ? parseInt(value) >= 1 && parseInt(value) <= 9007199254740991 : true;
+            })
+                .withMessageKey('errors:maxSupply')
             .on(CreateNft);
+    }
+
+    attached() {
+        this.authorisedIssuingAccounts.push({ name: '' });
+        this.authorisedIssuingContracts.push({ name: '' });
+    }
+
+    addAuthorisedAccount() {
+        this.authorisedIssuingAccounts.push({ name: '' });
+    }
+
+    addAuthorisedContract() {
+        this.authorisedIssuingContracts.push({ name: '' });
     }
 
     public async createToken() {
         const validationResult = await this.controller.validate();
 
-        const payload: { symbol: string; name: string; precision: number; maxSupply: number; url?: string } = {
+        const payload: { symbol: string; name: string; maxSupply?: number; url?: string, authorizedIssuingAccounts?: string[], authorisedIssuingContracts?: string[] } = {
             symbol: this.symbol,
-            name: this.tokenName,
-            precision: parseInt(this.precision),
-            maxSupply: parseInt(this.maxSupply),
+            name: this.tokenName
         };
 
         if (this.url !== null && this.url.trim() !== '') {
             payload.url = this.url;
+        }
+
+        if (this.maxSupply !== null && this.maxSupply.trim() !== '') {
+            payload.maxSupply = this.maxSupply;
+        }
+
+        if (this.authorisedIssuingAccounts.length) {
+            const accounts = this.authorisedIssuingAccounts.reduce((acc: string[], value) => {
+                acc.push(value.name);
+                return acc;
+            }, []);
+
+            if (accounts.length) {
+                payload.authorizedIssuingAccounts = accounts;
+            }
+        }
+
+        if (this.authorisedIssuingContracts.length) {
+            const accounts = this.authorisedIssuingContracts.reduce((acc: string[], value) => {
+                acc.push(value.name);
+                return acc;
+            }, []);
+
+            if (accounts.length) {
+                payload.authorisedIssuingContracts = accounts;
+            }
         }
 
         const userHasFunds = this.tokenCreationFee <= this.engBalance;
@@ -112,16 +146,16 @@ export class CreateNft {
         if (validationResult.valid && userHasFunds) {
             const result = await createTransaction(
                 this.state.account.name,
-                'tokens',
+                'nft',
                 'create',
                 payload,
-                'Steem Engine Token Registration',
-                'tokenCreateSuccess',
-                'tokenCreateError',
+                'Steem Engine NFT Creation',
+                'nftCreateSuccess',
+                'nftCreateError',
             );
 
             if (result !== false) {
-                this.router.navigateToRoute('exchange', { symbol: this.symbol })
+                this.router.navigateToRoute('nft', { symbol: this.symbol });
             }
         }
     }
