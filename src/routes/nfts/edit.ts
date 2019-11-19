7d6eda39c9a0c31eb47a9114c307324dced7519b
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-11-19 20:01:44 +1000

    feat(nfts): edit screen somewhat done

--- src/routes/nfts/edit.ts
+++ src/routes/nfts/edit.ts
@@ -1,9 +1,13 @@
+import { createTransaction } from 'common/functions';
+import { BootstrapFormRenderer } from 'resources/bootstrap-form-renderer';
+import { ValidationController, ValidationRules } from 'aurelia-validation';
+import { INft } from './../../store/state';
+import { Redirect } from 'aurelia-router';
 import { pluck } from 'rxjs/operators';
-import { NftPropertiesModal } from '../../modals/nft/nft-properties';
 import { DialogService } from 'aurelia-dialog';
 import { State } from 'store/state';
 import { SteemEngine } from 'services/steem-engine';
-import { autoinject, TaskQueue } from 'aurelia-framework';
+import { NewInstance, TaskQueue } from 'aurelia-framework';
 
 
 import { connectTo, dispatchify } from 'aurelia-store';
@@ -11,21 +15,117 @@ import { getNft } from 'store/actions';
 
 import styles from './edit.module.css';
 
-@autoinject()
-@connectTo((store) => store.state.pipe(pluck('nft')))
+@connectTo()
 export class EditNft {
+    private renderer: BootstrapFormRenderer;
+    private controller: ValidationController;
     private styles = styles;
     private state: State;
-    private token;
+    private token: INft;
 
-    constructor(private se: SteemEngine, private taskQueue: TaskQueue, private dialogService: DialogService) {}
+    static inject = [NewInstance.of(ValidationController), NewInstance.of(ValidationController), SteemEngine, TaskQueue, DialogService];
 
-    async activate({ symbol }) {
-        await dispatchify(getNft)(symbol);
+    constructor(private nameController: ValidationController, private metadataController: ValidationController, private se: SteemEngine, private taskQueue: TaskQueue, private dialogService: DialogService) {
+        this.renderer = new BootstrapFormRenderer();
+
+        this.nameController.addRenderer(this.renderer);
+        this.metadataController.addRenderer(this.renderer);
+    }
+
+    async canActivate({ symbol }) {
+        try {
+            await dispatchify(getNft)(symbol);
+        } catch {
+            return new Redirect('/wallet');
+        }
+    }
+
+    addValidationRules() {
+        const nameRules = ValidationRules.
+            ensure('name')
+            .required()
+            .withMessageKey('errors:required')
+            .maxLength(50)
+            .withMessageKey('errors:maximumLength50')
+            .satisfies((value: string) => {
+                return value?.match(/^[a-zA-Z0-9 ]*$/)?.length > 0 ?? false;
+            })
+            .withMessageKey('errors:requiredAlphaNumericSpaces').rules;
+
+        this.nameController.removeObject(this.token);
+        this.nameController.addObject(this.token, nameRules);
+    }
+
+    async updateName() {
+        const validationResult = await this.nameController.validate();
+
+        const nft = { ...this.state.nft };
+
+        if (validationResult.valid && nft.name !== this.token.name) {
+            const payload = {
+                symbol: nft.symbol,
+                name: this.token.name
+            };
+
+            const result = await createTransaction(
+                this.state.account.name,
+                'nft',
+                'updateName',
+                payload,
+                'Update Name ',
+                'updateSuccess',
+                'updateError',
+            );
+
+            if (result !== false) {
+                window.location.reload();
+            }
+        }
+    }
+
+    async updateMetadata() {
+        const validationResult = await this.metadataController.validate();
+
+        const nft = { ...this.state.nft };
+        const payload = {} as Partial<INft>;
+
+        if (nft.metadata.url !== this.token.metadata.url) {
+            payload.metadata.url = this.token.metadata.url;
+        }
+
+        if (nft.metadata.icon !== this.token.metadata.icon) {
+            payload.metadata.icon = this.token.metadata.icon;
+        }
+
+        if (nft.metadata.desc !== this.token.metadata.desc) {
+            payload.metadata.desc = this.token.metadata.desc;
+        }
+
+        if (JSON.stringify(payload).length > 1000) {
+            window.alert('Your payload size was greater than 1000 characters. Please reduce the size of your description, URL or ICON location.')
+            return;
+        }
+
+        if (validationResult.valid) {
+            const result = await createTransaction(
+                this.state.account.name,
+                'nft',
+                'updateMetadata',
+                payload,
+                'Update Metadata ',
+                'updateSuccess',
+                'updateError',
+            );
+
+            if (result !== false) {
+                window.location.reload();
+            }
+        }
     }
 
     stateChanged(newState) {
-        this.token = { ...newState };
+        this.token = { ...newState.nft };
+        this.addValidationRules();
     }
-    
+
 }
