16211b2eddfbda47703ac1c41306cd9e6de86a28
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-11-19 15:25:05 +1000

    feat(environments): add chain id to stage

--- aurelia_project/environments/stage.ts
+++ aurelia_project/environments/stage.ts
@@ -4,7 +4,7 @@ export const environment: Partial<EnvironmentInterface> = {
     ...baseEnvironmentConfiguration,
     debug: false,
     testing: false,
-    chainId: 'ssc-00000000000000000002',
+    chainId: 'ssc-testnet1',
     GRAPHQL_API: 'https://graphql-qa.steem.services/',
     RPC_URL: 'https://testapi.steem-engine.com/',
     NODE_API_URL: 'https://node-api.steem-engine.com/v1/',
