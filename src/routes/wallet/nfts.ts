35eed00349b382dab91eb55f77f8a4c774522364
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-11-19 16:22:29 +1000

    feat(nfts): show user owned nfts on wallet screen

--- src/routes/wallet/nfts.ts
+++ src/routes/wallet/nfts.ts
@@ -1,3 +1,5 @@
+import { NftPropertiesModal } from './../../modals/nft/nft-properties';
+import { DialogService } from 'aurelia-dialog';
 import { State } from 'store/state';
 import { connectTo, dispatchify } from 'aurelia-store';
 import { SteemEngine } from 'services/steem-engine';
@@ -10,11 +12,17 @@ export class MyNfts {
     private state: State;
     private loading = false;
     
-    constructor(private se: SteemEngine) {
+    constructor(private se: SteemEngine, private dialogService: DialogService) {
 
     }
 
     async activate() {
         await dispatchify(getUserNfts)();
+    }
+
+    showNftProperties(properties) {
+        this.dialogService.open({ viewModel: NftPropertiesModal, model: properties }).whenClosed(response => {
+            //console.log(response);
+        });
     }
 }
