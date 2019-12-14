34f54a70929dbc2782df604b7bca194ddbb3de73
horlorlahdeh <horlorlahdeh@gmail.com>
2019-12-14 04:54:56 +0100

    nft-detail router-view stuff

--- src/routes/nfts/nft-detail/nft-detail.ts
+++ src/routes/nfts/nft-detail/nft-detail.ts
@@ -1,12 +1,12 @@
-// import { DialogService } from 'aurelia-dialog';
-// import { NftPropertiesModal } from './../../modals/nft/nft-properties';
+import { PLATFORM } from 'aurelia-framework';
+import { Router, RouterConfiguration } from 'aurelia-router';
 import { State } from 'store/state';
 import { SteemEngine } from 'services/steem-engine';
 import { autoinject, TaskQueue } from 'aurelia-framework';
 import { slick } from 'slick-carousel/slick/slick';
-
-
-
+import ImageZoom from 'js-image-zoom/js-image-zoom';
+import { faStar } from '@fortawesome/pro-duotone-svg-icons';
+import { bindable } from 'aurelia-framework';
 import { connectTo, dispatchify } from 'aurelia-store';
 import { getNft, getNftInstance } from 'store/actions';
 
@@ -15,12 +15,42 @@ import styles from './nft-detail.module.css';
 @autoinject()
 @connectTo()
 export class nftDetail {
+    @bindable iconStar = faStar;
+    private router: Router;
     private styles = styles;
     private state: State;
     private Slick = slick;
     private tokenTable: HTMLTableElement;
+    private options = {
+        width: 650,
+        height: 350,
+        zoomWidth: 500,
+        offset: { vertical: 0, horizontal: 10 },
+    };
 
     constructor(private se: SteemEngine, private taskQueue: TaskQueue) {}
+    public configureRouter(config: RouterConfiguration, router: Router) {
+        config.map([
+            {
+                route: [''],
+                name: 'nft-overview',
+                moduleId: PLATFORM.moduleName('./nft-detail-full/nft-overview'),
+                nav: true,
+                title: 'NFT Overview',
+            },
+            {
+                route: ['nft-reviews'],
+                name: 'nft-reviews',
+                moduleId: PLATFORM.moduleName('./nft-detail-full/nft-reviews'),
+                nav: true,
+                title: 'NFT Reviews',
+            }
+        ]);
 
-
+        this.router = router;
+    }
+    attached() {
+        console.log(ImageZoom.zoomLensStyle);
+        ImageZoom(document.getElementById('container'), this.options);
+    }
 }
