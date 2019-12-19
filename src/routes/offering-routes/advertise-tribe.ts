78fab964541326764fe4bc75f94b720050ea3c00
horlorlahdeh <horlorlahdeh@gmail.com>
2019-12-19 21:36:18 +0100

    Advertise on SE landing page additional sections

--- src/routes/offering-routes/advertise-tribe.ts
+++ src/routes/offering-routes/advertise-tribe.ts
@@ -1,5 +1,11 @@
-import styles from "./advertise-tribe.module.css";
+import styles from './advertise-tribe.module.css';
 
 export class advertiseTribes {
     private styles = styles;
+    handleClick(e) {
+        $('.faq-content').css('display', 'none');
+        $('#faq' + e + 'Content').css('display', 'block');
+        // $('.faq-item' + e ).addClass('active');
+        // $('.faq-items').removeClass('active');
+    }
 }
