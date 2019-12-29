0814d42d3dea5d63b589b1050c179bf77610e615
Dwayne Charrington <dwaynecharrington@gmail.com>
2019-12-29 13:07:23 +1000

    feat(app): inverse validation check for token

--- src/common/custom-validation-rules.ts
+++ src/common/custom-validation-rules.ts
@@ -4,9 +4,9 @@ import { ValidationRules } from 'aurelia-validation';
 async function nftExists(symbol: string) {
     const response = await query(`nft(symbol: "${symbol}") { symbol }`);
 
-    return response.data.nft !== null;
+    return response.data.nft !== null ? false : true;
 }
 
-ValidationRules.customRule('nft-token', async (value, obj) => {
+ValidationRules.customRule('nftAvailable', async (value, obj) => {
     return await nftExists(value);
 }, 'Token already exists');
