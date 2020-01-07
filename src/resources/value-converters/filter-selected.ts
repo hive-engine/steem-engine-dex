ebd13c46935d9816c9aae4c833b70f52972c3740
Dwayne Charrington <dwaynecharrington@gmail.com>
2020-01-07 22:35:51 +1000

    feat(value-converters): allow boolean value for checks

--- src/resources/value-converters/filter-selected.ts
+++ src/resources/value-converters/filter-selected.ts
@@ -1,9 +1,15 @@
 export class FilterSelectedValueConverter {
-    toView(arr, properties) {
+    toView(arr, properties, returnBool = false) {
         if (!arr) {
             return arr;
         }
 
-        return arr.filter(val => !properties.some(prop => prop.name === val));
+        const filtered = arr.filter(val => !properties.some(prop => prop.name === val));
+
+        if (returnBool) {
+            return filtered.length;
+        }
+
+        return filtered;
     }
 }
