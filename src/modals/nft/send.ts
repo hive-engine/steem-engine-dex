6d171436fa14d88abbb51a011a748f547c61f1eb
Dwayne Charrington <dwaynecharrington@gmail.com>
2020-01-15 21:06:17 +1000

    feat(nfts): transfer nft modal

---
+++ src/modals/nft/send.ts
@@ -0,0 +1,17 @@
+import { DialogController } from 'aurelia-dialog';
+import { autoinject } from 'aurelia-framework';
+
+@autoinject()
+export class SendNftModal {
+    private token;
+    private transaction;
+
+    constructor(private controller: DialogController) {
+        this.controller.settings.lock = false;
+        this.controller.settings.centerHorizontalOnly = true;
+    }
+
+    activate(token) {
+        this.token = token;
+    }
+}
