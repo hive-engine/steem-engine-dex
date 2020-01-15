b7be534441ca1241a69a357249abbeef3435e579
Dwayne Charrington <dwaynecharrington@gmail.com>
2020-01-15 21:53:09 +1000

    feat(nfts): burn action

--- src/routes/wallet/nfts.ts
+++ src/routes/wallet/nfts.ts
@@ -1,3 +1,6 @@
+import { sleep } from 'common/functions';
+import { checkTransaction } from 'common/steem-engine';
+import { NftService } from './../../services/nft-service';
 import { NftTransferModal } from './../../modals/nft/nft-transfer';
 import { NftPropertiesModal } from './../../modals/nft/nft-properties';
 import { DialogService } from 'aurelia-dialog';
@@ -5,15 +8,16 @@ import { State } from 'store/state';
 import { connectTo, dispatchify } from 'aurelia-store';
 import { SteemEngine } from 'services/steem-engine';
 import { autoinject } from 'aurelia-framework';
-import { getUserNfts } from 'store/actions';
+import { getUserNfts, loading } from 'store/actions';
 
 @autoinject()
 @connectTo()
 export class MyNfts {
     private state: State;
     private loading = false;
+    private errors: string[] = [];
     
-    constructor(private se: SteemEngine, private dialogService: DialogService) {
+    constructor(private se: SteemEngine, private nftService: NftService, private dialogService: DialogService) {
 
     }
 
@@ -33,7 +37,29 @@ export class MyNfts {
         });
     }
 
-    burnNft(token) {
-        
+    async burnNft(token) {
+        dispatchify(loading)(true);
+
+        try {
+            const transfer = await this.nftService.burn(token.symbol, token._id) as any;
+
+            if (transfer.success) {
+                try {
+                    const verify = await checkTransaction(transfer.result.id, 3);
+                    
+                    if (verify?.errors) {
+                        this.errors = verify.errors;
+                    } else {
+                        await sleep(3200);
+                    }
+                } catch (e) {
+                    console.error(e);
+                }
+            }
+
+            dispatchify(loading)(false);
+        } catch {
+            dispatchify(loading)(false);
+        }
     }
 }
