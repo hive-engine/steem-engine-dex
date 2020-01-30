62d1ff808d495aca4decfb8f7ebd24554fad652d
lion-200 <i.duz86@outlook.com>
2020-01-30 21:55:16 +0100

    - Bugfixing "Unable to transfer NFTs" issue related to empty "to" field in custom json - Loading implemented in NFT Transfer & NFT Sell popups - Validation implemented in NFT Transfer & NFT Sell popups - Error messages added to locales

--- src/modals/nft/nft-transfer.ts
+++ src/modals/nft/nft-transfer.ts
@@ -2,51 +2,103 @@ import { checkTransaction } from 'common/steem-engine';
 import { sleep } from 'common/functions';
 import { loading } from 'store/actions';
 import { dispatchify } from 'aurelia-store';
-import { NftService } from './../../services/nft-service';
+import { NftService } from 'services/nft-service';
 import { DialogController } from 'aurelia-dialog';
 import { autoinject } from 'aurelia-framework';
+import { ToastService, ToastMessage } from 'services/toast-service';
+import { ValidationControllerFactory, ControllerValidateResult, ValidationRules } from 'aurelia-validation';
+import { I18N } from 'aurelia-i18n';
+import { BootstrapFormRenderer } from 'resources/bootstrap-form-renderer';
 
 @autoinject()
 export class NftTransferModal {
     private token;
-    private transaction;
+    private transactionTo;
+    private transactionToType;
     private errors: string[] = [];
+    private loading = false;
+    private validationController;
+    private renderer;
 
-    constructor(private controller: DialogController, private nftService: NftService) {
+    constructor(private controller: DialogController, private nftService: NftService, private toast: ToastService, private controllerFactory: ValidationControllerFactory, private i18n: I18N) {
+        this.validationController = controllerFactory.createForCurrentScope();
+
+        this.renderer = new BootstrapFormRenderer();
+        this.validationController.addRenderer(this.renderer);
+
         this.controller.settings.lock = false;
         this.controller.settings.centerHorizontalOnly = true;
     }
 
     activate(token) {
         this.token = token;
+        this.transactionToType = "user";
         console.log(token);
     }
 
+    bind() {
+        this.createValidationRules();
+    }
+
+    private createValidationRules() {
+        const rules = ValidationRules            
+            .ensure('transactionTo')
+            .required()
+            .withMessageKey('errors:nftTransferToRequired')
+            .ensure('transactionToType')
+            .required()
+            .withMessageKey('errors:nftTransferToTypeRequired')
+            .rules;
+
+        this.validationController.addObject(this, rules);
+    }
+
     async transfer() {
-        dispatchify(loading)(true);
+        this.loading = true;
 
         try {
-            const transfer = await this.nftService.transfer(this.token.symbol, this.token._id, this.transaction.id, this.transaction.toType) as any;
+            const validationResult: ControllerValidateResult = await this.validationController.validate();
 
-            if (transfer.success) {
-                try {
-                    const verify = await checkTransaction(transfer.result.id, 3);
-                    
-                    if (verify?.errors) {
-                        this.errors = verify.errors;
-                    } else {
-                        await sleep(3200);
-                        
-                        window.location.reload();
-                    }
-                } catch (e) {
-                    console.error(e);
+            this.loading = true;
+
+            for (const result of validationResult.results) {
+                if (!result.valid) {
+                    const toast = new ToastMessage();
+
+                    toast.message = this.i18n.tr(result.rule.messageKey, {
+                        to: this.transactionTo,
+                        toType: this.transactionToType,
+                        symbol: this.token.symbol,
+                        ns: 'errors'
+                    });
+
+                    this.toast.error(toast);
                 }
             }
 
-            dispatchify(loading)(false);
+            if (validationResult.valid) {
+                const transfer = await this.nftService.transfer(this.token.symbol, this.token._id, this.transactionTo, this.transactionToType) as any;
+
+                if (transfer.success) {
+                    try {
+                        const verify = await checkTransaction(transfer.result.id, 3);
+
+                        if (verify?.errors) {
+                            this.errors = verify.errors;
+                        } else {
+                            await sleep(3200);
+
+                            window.location.reload();
+                        }
+                    } catch (e) {
+                        console.error(e);
+                    }
+                }
+            }
+
+            this.loading = false;
         } catch {
-            dispatchify(loading)(false);
+            this.loading = false;
         }
     }
 }
