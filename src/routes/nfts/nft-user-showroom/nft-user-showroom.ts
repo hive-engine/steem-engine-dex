9f2668d71968a8476b2ceed222b684b91afac3a7
horlorlahdeh <horlorlahdeh@gmail.com>
2020-02-04 02:25:43 +0100

    user showroom customizable

---
+++ src/routes/nfts/nft-user-showroom/nft-user-showroom.ts
@@ -0,0 +1,25 @@
+import { SteemEngine } from 'services/steem-engine';
+import { autoinject, TaskQueue } from 'aurelia-framework';
+import { DialogService, DialogCloseResult } from 'aurelia-dialog';
+import { connectTo, dispatchify } from 'aurelia-store';
+import { getNftsWithSellBook } from 'store/actions';
+
+import { slick } from 'slick-carousel/slick/slick';
+import styles from './nft-user-showroom.module.css';
+
+@autoinject()
+@connectTo()
+export class nftUserShowroom {
+    private styles = styles;
+    private state: State;
+    private Slick = slick;
+    private tokenTable: HTMLTableElement;
+
+    constructor(private se: SteemEngine, private taskQueue: TaskQueue) {}
+
+    async canActivate() {
+        await dispatchify(getNftsWithSellBook)();
+    }
+
+    
+}
