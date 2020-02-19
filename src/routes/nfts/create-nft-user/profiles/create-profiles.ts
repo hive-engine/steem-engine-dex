bbad46cb4167bbcdf4a76c70bb3a34d515f9353f
horlorlahdeh <horlorlahdeh@gmail.com>
2020-02-19 18:25:12 +0100

    create profiles options view

---
+++ src/routes/nfts/create-nft-user/profiles/create-profiles.ts
@@ -0,0 +1,9 @@
+import { customElement, autoinject, bindable } from 'aurelia-framework';
+
+import styles from './create-profiles.module.css';
+
+@autoinject()
+@customElement('createprofiles')
+export class CreateProfiles {
+    private styles = styles;
+}
