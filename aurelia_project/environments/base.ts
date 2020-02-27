e150f52668eb19ec145a26aa87562fe2d238a7b8
Dwayne Charrington <dwaynecharrington@gmail.com>
2020-02-27 22:39:38 +1000

    chore(tokens): ban scam token ether

--- aurelia_project/environments/base.ts
+++ aurelia_project/environments/base.ts
@@ -7,7 +7,7 @@ export const baseEnvironmentConfiguration: Partial<EnvironmentInterface> = {
     CONVERTER_API: 'https://converter-api.steem-engine.com/api/',
     FIREBASE_API: 'https://us-central1-steem-engine-dex.cloudfunctions.net/api/',
     maintenanceMode: false,
-    disabledTokens: ['BTC', 'LTC', 'STEEM', 'SBD', 'BCC', 'XAP', 'XRP', 'GOLOS', 'DISNEY', 'AMAZON', 'VOICE', 'ETH', 'EOS', 'LASSE', 'TIME', 'R', 'SCTR', 'ALLAH', 'BNB', 'DONE'],
+    disabledTokens: ['BTC', 'LTC', 'STEEM', 'SBD', 'BCC', 'XAP', 'XRP', 'GOLOS', 'DISNEY', 'AMAZON', 'VOICE', 'ETH', 'EOS', 'LASSE', 'TIME', 'R', 'SCTR', 'ALLAH', 'BNB', 'DONE', 'ETHER'],
     peggedToken: 'STEEMP',
     features: {
         nfts: {
