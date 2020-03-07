3aad507236c7f8c04eceff17038f714e4ea60838
horlorlahdeh <horlorlahdeh@gmail.com>
2020-03-07 07:02:46 +0100

    Deleted files as files were moved

--- src/routes/nfts/create-nft-user/profiles/profile-forms/core-profile.ts
+++
@@ -1,9 +0,0 @@
-import { customElement, autoinject, bindable } from 'aurelia-framework';
-
-import styles from './core-profile.module.css';
-
-@autoinject()
-@customElement('coreProfile')
-export class CoreProfile {
-    private styles = styles;
-}
