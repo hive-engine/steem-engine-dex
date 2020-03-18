a798a75abec52129f7fe180a17aeac5e8bfc8f36
horlorlahdeh <horlorlahdeh@gmail.com>
2020-03-18 19:24:23 +0100

    All user collection

--- src/routes/nfts/create-nft-user/profiles/profile-page/components/all-user-collection.ts
+++ src/routes/nfts/create-nft-user/profiles/profile-page/components/all-user-collection.ts
@@ -1,12 +1,13 @@
 import { SteemEngine } from 'services/steem-engine';
 import { autoinject, TaskQueue } from 'aurelia-framework';
 import { DialogService, DialogCloseResult } from 'aurelia-dialog';
+import { customElement, bindable } from 'aurelia-framework';
 import { connectTo, dispatchify } from 'aurelia-store';
 import { getNftsWithSellBook } from 'store/actions';
 
 
 import { slick } from 'slick-carousel/slick/slick';
-import styles from '../nft-user-showroom.module.css';
+import styles from './all-user-collection.module.css';
 
 @autoinject()
 @connectTo()
@@ -17,7 +18,7 @@ export class allUserCollection {
     private tokenTable: HTMLTableElement;
 
     constructor(private se: SteemEngine, private taskQueue: TaskQueue) {}
-    
+
     async canActivate() {
         await dispatchify(getNftsWithSellBook)();
     }
