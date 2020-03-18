cdfd4f3e249bf935553009e103883fe399469ba3
horlorlahdeh <horlorlahdeh@gmail.com>
2020-03-18 19:24:38 +0100

    User profile

--- src/routes/nfts/create-nft-user/profiles/profile-page/user-profile.ts
+++ src/routes/nfts/create-nft-user/profiles/profile-page/user-profile.ts
@@ -19,28 +19,38 @@ export class nftUserShowroom {
     private tokenTable: HTMLTableElement;
 
     constructor(private se: SteemEngine, private taskQueue: TaskQueue) {}
-    // public configureRouter(config: RouterConfiguration, router: Router) {
-    //     config.map([
-    //         {
-    //             route: [''],
-    //             name: 'allCollections',
-    //             moduleId: PLATFORM.moduleName('./components/all-user-collection'),
-    //             nav: true,
-    //             title: 'All',
-    //         },
-    //         {
-    //             route: ['user-reviews'],
-    //             name: 'userReviews',
-    //             moduleId: PLATFORM.moduleName('./components/user-reviews'),
-    //             nav: true,
-    //             title: 'User Reviews',
-    //         },
-    //     ]);
+    public configureRouter(config: RouterConfiguration, router: Router) {
+        config.map([
+            {
+                route: [''],
+                name: 'allCollections',
+                moduleId: PLATFORM.moduleName('./components/all-user-collection'),
+                nav: true,
+                title: 'All',
+            },
+            {
+                route: ['directory'],
+                name: 'directory',
+                moduleId: PLATFORM.moduleName('./components/directory'),
+                nav: true,
+                title: 'Directory',
+            },
+        ]);
 
-    //     this.router = router;
-    // }
-
-    async canActivate() {
-        await dispatchify(getNftsWithSellBook)();
+        this.router = router;
     }
+
+    showMore() {
+        $('#more-less').css('display', 'block');
+        $('#more-btn').css('display', 'none');
+        $('#less-btn').css('display', 'block');
+        console.log('here is more')
+    }
+    showLess() {
+        $('#more-less').css('display', 'none');
+        $('#more-btn').css('display', 'block');
+        $('#less-btn').css('display', 'none');
+        console.log('here is more')
+    }
+
 }
