77694c33c244160735aceb27330f691ce73c9373
horlorlahdeh <horlorlahdeh@gmail.com>
2020-03-20 09:32:23 +0100

    Active class to bubbles

--- src/routes/nfts/create-nft-user/create-listing-nav/stepsnav.ts
+++ src/routes/nfts/create-nft-user/create-listing-nav/stepsnav.ts
@@ -14,8 +14,12 @@ export class StepsNav {
         $('#tab-' + n).css('display', 'block');
         this.clicked = n;
         let next = n + 1;
+        let nextNext = n + 2;
+        let prevPrev = n - 2;
         let prev = n - 1;
         $('#step-' + next).removeClass('active-form');
+        $('#step-' + nextNext).removeClass('active-form');
+        $('#step-' + prevPrev).addClass('active-form');
         $('#step-' + prev).addClass('active-form');
         
         console.log(`${next} and ${prev}`)
