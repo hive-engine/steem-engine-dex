17465e2dfc2e37c50ecd4da31a80c5d84b3afdba
horlorlahdeh <horlorlahdeh@gmail.com>
2020-04-16 02:52:30 +0100

    Directory Change

--- src/routes/nfts/create-nft-user/profiles/user-profile/edit-profile/edit-profile.ts
+++ src/routes/nfts/create-nft-user/profiles/user-profile/edit-profile/edit-profile.ts
@@ -3,7 +3,7 @@ import { customElement, autoinject, bindable } from 'aurelia-framework';
 import styles from './edit-profile.module.css';
 
 @autoinject()
-@customElement('editProfile')
+@customElement('editprofile')
 export class CoreProfile {
     private styles = styles;
 }
