a1b6c6f1bede6485e2b210970da3b049e7e160c4
horlorlahdeh <horlorlahdeh@gmail.com>
2020-04-30 21:00:20 +0100

    DSwap Login Route

---
+++ src/routes/dswap/dswap-login.ts
@@ -0,0 +1,11 @@
+import { customElement, autoinject, bindable } from 'aurelia-framework';
+
+import styles from './dswap-login.module.css';
+
+
+
+
+@autoinject()
+export class Dswap {
+    private styles = styles;
+}
