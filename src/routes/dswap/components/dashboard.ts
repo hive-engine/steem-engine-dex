773bf6311c4902eccca57a4b479a404e8a583ea3
horlorlahdeh <horlorlahdeh@gmail.com>
2020-05-03 05:03:28 +0100

    Dashboard for DSwap

--- src/routes/dswap/components/dashboard.ts
+++ src/routes/dswap/components/dashboard.ts
@@ -1,4 +1,7 @@
 import { customElement, autoinject, bindable } from 'aurelia-framework';
+import { ChartComponent } from '../../../components/chart/chart';
+import { loadTokenMarketHistory } from 'common/steem-engine';
+import moment from 'moment';
 
 import styles from "../dswap.module.css";
 
@@ -6,4 +9,6 @@ import styles from "../dswap.module.css";
 @customElement('dashboard')
 export class Dashboard {
     private styles = styles;
+    
+    
 }
