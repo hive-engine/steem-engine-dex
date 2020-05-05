bbb385fc0be7faceba48df1934a6d50ca4dd6eb0
lion-200 <i.duz86@outlook.com>
2020-05-06 01:17:48 +0200

    Fix broken unit test after changing steem price endpoint

--- test/unit/functions.spec.ts
+++ test/unit/functions.spec.ts
@@ -160,7 +160,7 @@ describe('Functions', () => {
     });
 
     test('getSteemPrice should return mock steem price', async () => {
-        fetchMock.mockResponseOnce(JSON.stringify({ steem_price: 0.389283 }));
+        fetchMock.mockResponseOnce(JSON.stringify({ steem: { usd: 0.389283 } }));
 
         const response = await getSteemPrice();
         expect(response).toEqual(0.389283)
