8c72e7f9efcf3f9274b333fc273aebb158a4d72d
horlorlahdeh <horlorlahdeh@gmail.com>
2020-05-08 20:36:19 +0100

    Trade Screen

--- src/routes/dswap/components/trade.ts
+++ src/routes/dswap/components/trade.ts
@@ -1,4 +1,6 @@
 import { customElement, autoinject, bindable } from 'aurelia-framework';
+import { DswapOrderModal } from '../modals/dswap-order';
+import { DialogService } from 'aurelia-dialog';
 
 import styles from "../dswap.module.css";
 
@@ -6,4 +8,12 @@ import styles from "../dswap.module.css";
 @customElement('trade')
 export class Trade {
     private styles = styles;
+
+    constructor(private dialogService: DialogService) {}
+
+    withdraw() {
+        this.dialogService.open({ viewModel: DswapOrderModal }).whenClosed(response => {
+            console.log(response);
+        });
+    }
 }
