08d875da2685441e276d8a599eeac3283bbf5cee
horlorlahdeh <horlorlahdeh@gmail.com>
2020-05-11 16:48:24 +0100

    Dashboard Route

--- src/routes/dswap/components/dashboard.ts
+++ src/routes/dswap/components/dashboard.ts
@@ -1,4 +1,6 @@
 import { customElement, autoinject, bindable } from 'aurelia-framework';
+import { DswapOrderModal } from '../modals/dswap-order';
+import { DialogService } from 'aurelia-dialog';
 
 
 import styles from "../dswap.module.css";
@@ -6,7 +8,12 @@ import styles from "../dswap.module.css";
 @autoinject()
 @customElement('dashboard')
 export class Dashboard {
+    constructor(private dialogService: DialogService) {}
     private styles = styles;
 
-    
+    withdraw() {
+        this.dialogService.open({ viewModel: DswapOrderModal }).whenClosed(response => {
+            console.log(response);
+        });
+    }
 }
