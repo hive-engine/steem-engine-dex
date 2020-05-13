71cdd418aea6bed3b271a95887b09f7051a86d1f
lion-200 <i.duz86@outlook.com>
2020-05-13 14:48:57 +0200

    remove PAL

--- src/services/nft-service.ts
+++ src/services/nft-service.ts
@@ -10,7 +10,7 @@ import moment from 'moment';
 import { ToastService, ToastMessage } from './toast-service';
 import { I18N } from 'aurelia-i18n';
 
-type NftFees = 'ENG';
+type NftFees = 'ENG'; 
 type NftType = 'contract' | 'user';
 
 @autoinject()
