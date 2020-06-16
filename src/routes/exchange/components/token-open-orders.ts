e018c9c5f0ca73200e8a9f215e9b993fceb46d15
lion-200 <i.duz86@outlook.com>
2020-06-16 21:16:54 +0200

    Bulk cancellation wallet open orders & exchange open orders block

--- src/routes/exchange/components/token-open-orders.ts
+++ src/routes/exchange/components/token-open-orders.ts
@@ -2,11 +2,16 @@ import { customElement, bindable } from 'aurelia-framework';
 import { cancelMarketOrder } from 'common/market';
 import { SteemEngine } from 'services/steem-engine';
 import { EventAggregator } from 'aurelia-event-aggregator';
+import { faInfo } from '@fortawesome/pro-regular-svg-icons';
+import { ToastService, ToastMessage } from 'services/toast-service';
+import { I18N } from 'aurelia-i18n';
 
 @customElement('token-open-orders')
 export class TokenOpenOrders {
     @bindable() loading = true;
     @bindable() orders;
+    private anyOrderChecked = false;
+    @bindable iconInfo = faInfo;
 
     private eventAggregator: EventAggregator;
     private reloadEventData: IReloadEventData = {
@@ -17,7 +22,7 @@ export class TokenOpenOrders {
         reloadTokenOpenOrders: false
     };
 
-    constructor(private se: SteemEngine, private ea: EventAggregator) {
+    constructor(private se: SteemEngine, private ea: EventAggregator, private i18n: I18N, private toastService: ToastService) {
         this.eventAggregator = ea;
     }
 
@@ -25,22 +30,73 @@ export class TokenOpenOrders {
         this.loading = false;
     }
 
+    publishReloadEvent(type) {
+        this.reloadEventData.reloadUserExchangeData = true;
+        this.reloadEventData.reloadTokenOpenOrders = true;
+
+        if (!type) {
+            this.reloadEventData.reloadBuyBook = true;
+            this.reloadEventData.reloadSellBook = true;
+        } else if (type === "buy") {
+            this.reloadEventData.reloadBuyBook = true;
+        } else {
+            this.reloadEventData.reloadSellBook = true;
+        }
+
+        this.eventAggregator.publish('eventReload', { data: this.reloadEventData });
+    }
+
     async cancelOrder(type: string, txId: string, symbol: string) {
         this.loading = true;
         const res: any = await cancelMarketOrder(this.se.getUser(), type, txId, symbol);
                 
         if (res && res.transactionId) {
-            this.reloadEventData.reloadUserExchangeData = true;
-            this.reloadEventData.reloadTokenOpenOrders = true;
-
-            if (type === "buy") {
-                this.reloadEventData.reloadBuyBook = true;
-            } else {
-                this.reloadEventData.reloadSellBook = true;
-            }
-
-            this.eventAggregator.publish('eventReload', { data: this.reloadEventData });
+            this.publishReloadEvent(type);
         }
+        this.loading = false;
+    }
+
+    async isAnyOrderChecked() {
+        let ordersChecked = this.orders.find(x => x.checked === true);
+
+        if (ordersChecked)
+            this.anyOrderChecked = true;
+        else
+            this.anyOrderChecked = false;
+    }
+
+    async orderCheckChange() {
+        this.isAnyOrderChecked();
+    }
+
+    async cancelSelectedOrders() {
+        let reloadOrders = false;
+        let orders = this.orders.filter(x => x.checked === true);
+
+        if (window.steem_keychain) {
+            if (orders.length > 0) {
+                this.loading = true;
+
+                for (let o of orders) {
+                    let response: any = await cancelMarketOrder(this.se.getUser(), o.type, o.txId, o.symbol);
+
+                    if (response && response.transactionId)
+                        reloadOrders = true;
+                }
+            }
+        } else {
+            var toast = new ToastMessage();
+
+            toast.message = this.i18n.tr('orderCancelSelectedOnlyKeychain', {
+                ns: 'notifications'
+            });
+
+            this.toastService.error(toast);
+        }
+
+        if (reloadOrders)
+            this.publishReloadEvent(null);
+
         this.loading = false;
     }
 }
--- src/routes/wallet/open-orders.ts
+++ src/routes/wallet/open-orders.ts
@@ -1,15 +1,18 @@
 import { getUserOpenOrders, cancelMarketOrder } from 'common/market';
 import { SteemEngine } from 'services/steem-engine';
 import { autoinject } from 'aurelia-framework';
+import { ToastMessage, ToastService } from 'services/toast-service';
+import { I18N } from 'aurelia-i18n';
 
 @autoinject()
 export class OpenOrders {
     private loadingOpenOrders = false;
     private orders = [];
+    private anyOrderChecked = false;
 
     private openOrdersTable: HTMLTableElement;
     
-    constructor(private se: SteemEngine) {
+    constructor(private se: SteemEngine, private i18n: I18N, private toastService: ToastService) {
 
     }
 
@@ -48,6 +51,50 @@ export class OpenOrders {
     }
 
     async cancelOrder(type: string, txId: string, symbol: string) {
-        await cancelMarketOrder(this.se.getUser(), type, txId, symbol);
+        return await cancelMarketOrder(this.se.getUser(), type, txId, symbol);
+    }
+
+    async isAnyOrderChecked() {
+        let ordersChecked = this.orders.find(x => x.checked === true);
+
+        if (ordersChecked)
+            this.anyOrderChecked = true;
+        else
+            this.anyOrderChecked = false;
+    }
+
+    async orderCheckChange() {
+        this.isAnyOrderChecked();
+    }
+
+    async cancelSelectedOrders() {
+        let reloadOrders = false;
+        let orders = this.orders.filter(x => x.checked === true);
+
+        if (window.steem_keychain) {
+            if (orders.length > 0) {
+                this.loadingOpenOrders = true;
+
+                for (let o of orders) {
+                    let response: any = await this.cancelOrder(o.type, o.txId, o.symbol);
+                    
+                    if (response && response.transactionId)
+                        reloadOrders = true;
+                }
+            }
+        } else {
+            var toast = new ToastMessage();
+
+            toast.message = this.i18n.tr('orderCancelSelectedOnlyKeychain', {
+                ns: 'notifications'
+            });
+
+            this.toastService.error(toast);
+        }
+
+        if (reloadOrders)
+            this.orders = await getUserOpenOrders(this.se.getUser());
+
+        this.loadingOpenOrders = false;
     }
 }
